33105da0f2f877c83bed5e77b843945be509d8fb
shuoshuof <1587097352@qq.com>
2024-10-21 11:37:59 +0800

    add

--- motion_convert/pipeline/smpl2hu_pipeline.py
+++ motion_convert/pipeline/smpl2hu_pipeline.py
@@ -1,11 +1,12 @@
 import os
 from tqdm import tqdm
-import math
-import torch
+import copy
 from abc import ABC, abstractmethod
 from typing import Optional, Union
 import joblib
 import pickle
+import math
+import torch
 import numpy as np
 from scipy.spatial.transform import Rotation as sRot
 
@@ -16,6 +17,7 @@ from poselib.poselib.visualization.common import *
 from body_visualizer.body_visualizer import BodyVisualizer
 from motion_convert.pipeline.base_pipeline import BasePipeline
 from motion_convert.format_convert.smpl2isaac import convert2isaac
+from motion_convert.utils.rotation import *
 
 JOINT_MAPPING_v1 = {
     'Pelvis': 'pelvis_link',
@@ -40,41 +42,52 @@ JOINT_MAPPING_v1 = {
 class SMPL2HuPipeline(BasePipeline):
     def __init__(self, motion_dir: str, save_dir: str, num_processes: int = None):
         super().__init__(motion_dir, save_dir, num_processes)
-        self.smpl_t_pose,self.hu_t_pose = self._load_t_pose()
-        self.smpl_skeleton_tree = self.smpl_t_pose.skeleton_tree
-        self.hu_skeleton_tree = self.hu_t_pose.skeleton_tree
-        body_visualizer = BodyVisualizer('smpl24')
-        body_visualizer.step(self.smpl_t_pose.global_translation)
+        self.smpl_t_pose,self.hu_t_pose,self.smpl_skeleton_tree,self.hu_skeleton_tree = self._load_asset()
+        
     def _read_data(self, **kwargs) -> Optional[list]:
         motion_paths = [os.path.join(self.motion_dir, f) for f in os.listdir(self.motion_dir) if
                               os.path.isfile(os.path.join(self.motion_dir, f))]
         return motion_paths
     def _split_data(self,data,**kwargs)->Optional[list]:
         return np.array_split(data,self.num_processes)
-    def _load_t_pose(self)->[SkeletonState,SkeletonState]:
+    def _load_asset(self)->[SkeletonState, SkeletonState,SkeletonTree, SkeletonTree]:
         with open('asset/t_pose/smpl_t_pose.pkl','rb') as f:
             smpl_t_pose = pickle.load(f)
         with open('asset/t_pose/hu_t_pose.pkl','rb') as f:
             hu_t_pose = pickle.load(f)
-        return smpl_t_pose,hu_t_pose
+        with open('asset/smpl/smpl_skeleton_tree.pkl','rb') as f:
+            smpl_skeleton_tree = pickle.load(f)
+        hu_skeleton_tree = hu_t_pose.skeleton_tree
+        return smpl_t_pose,hu_t_pose,smpl_skeleton_tree,hu_skeleton_tree
     def _rebuild_with_smpl_t_pose(self,motion:SkeletonMotion):
         motion_fps = motion.fps
-        motion_global_translation = motion.global_translation
-        motion_local_translation = motion.local_translation
-        motion_length, num_keypoint, _ = motion_global_translation.shape
 
+        motion_global_translation = motion.global_translation
+        motion_length, num_keypoint, _ = motion_global_translation.shape
         new_motion_global_rotation = motion.global_rotation
         new_motion_root_translation = motion.root_translation
-        new_motion_parent_indices = self.smpl_t_pose.skeleton_tree.parent_indices
+
+        t_pose_local_translation = self.smpl_t_pose.local_translation.repeat(motion_length,1,1)
+        parent_indices = self.smpl_t_pose.skeleton_tree.parent_indices
 
         rebuild_indices = [16,17,18,21,22,23]
 
         for joint_index in rebuild_indices:
-            new_motion_global_rotation[:,joint_index,:] = 0
+            new_motion_global_rotation[:,parent_indices[joint_index],:] = \
+                quat_between_two_vecs(t_pose_local_translation[:,joint_index],motion_global_translation[:,joint_index]-motion_global_translation[:,parent_indices[joint_index]])
 
-    def cal_rotation(self,vec1,vec2):
+        new_skeleton_state = SkeletonState.from_rotation_and_root_translation(
+            self.smpl_t_pose.skeleton_tree,
+            new_motion_global_rotation,
+            new_motion_root_translation,
+            is_local=False
+        )
+        new_motion = SkeletonMotion.from_skeleton_state(new_skeleton_state,fps=motion_fps)
 
-        pass
+        rebuild_error = torch.abs(new_motion.global_translation-motion_global_translation).max()
+        print(f"Rebuild error :{rebuild_error:.5f}")
+
+        return new_motion
 
 
     def _process_data(self,data_chunk,results,process_idx,**kwargs):
@@ -94,9 +107,25 @@ class SMPL2HuPipeline(BasePipeline):
                 motion_root_translation,
                 is_local=False
             )
-            smpl_motion = SkeletonMotion.from_skeleton_state(smpl_motion,fps=motion_fps)
 
-            plot_skeleton_H([smpl_motion])
+            # plot_skeleton_H([smpl_motion.zero_pose(self.smpl_skeleton_tree)])
+
+            smpl_motion = SkeletonMotion.from_skeleton_state(smpl_motion,fps=motion_fps)
+            # TODO： 腰部旋转很可能有问题
+            rebuilt_motion = self._rebuild_with_smpl_t_pose(smpl_motion)
+
+            target_motion = copy.deepcopy(rebuilt_motion).retarget_to_by_tpose(
+                joint_mapping=JOINT_MAPPING_v1,
+                source_tpose=self.smpl_t_pose,
+                target_tpose=self.hu_t_pose,
+                rotation_to_target_skeleton=torch.Tensor([0,0,0,1]),
+                scale_to_target_skeleton=1.,
+            )
+
+
+
+            plot_skeleton_H([smpl_motion,rebuilt_motion,target_motion])
+
 
 
 
