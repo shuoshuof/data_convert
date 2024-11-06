ea7c3faa1ad832be5430e231dfa5ccb6ddf553a3
shuoshuof <1587097352@qq.com>
2024-11-06 20:25:43 +0800

    add vedo callback

--- motion_convert/collision_detection/visualization.py
+++ motion_convert/collision_detection/visualization.py
@@ -1,24 +1,77 @@
 import matplotlib.pyplot as plt
 import torch
 from vedo import *
-from typing import List
+from typing import List,Union
 from motion_convert.collision_detection.obb import OBB,OBBCollisionDetector
 from poselib.poselib.core.rotation3d import *
 import time
+# class ObbVisualizer:
+#     def __init__(self, obbs:List[OBB]):
+#         self.obbs_boxes = [ObbBox(obb) for obb in obbs]
+#
+#         world_frame =  Box(pos=[0,0,0],length=2,width=2,height=2).wireframe()
+#
+#         self.plotter = Plotter()
+#         self.plotter.add(world_frame)
+#         self.button = self.plotter.add_button(
+#             self._button_func,
+#             states=["\u23F5 Play  ", "\u23F8 Pause"],
+#             font="Kanopus",
+#             size=32,
+#         )
+#     def _button_func(self, evt):
+#         self.button.switch()
+#     def update_obbs_boxes(self,obbs:List[OBB]):
+#         for i,obb in enumerate(obbs):
+#             self.obbs_boxes[i].update(obb)
+#
+#     def render(self):
+#         self.plotter.show(*self.obbs_boxes, axes=1, viewup='z', interactive=False)
+
 class ObbVisualizer:
-    def __init__(self, obbs:List[OBB]):
-        self.obbs_boxes = [ObbBox(obb) for obb in obbs]
+    def __init__(self,obb_collision_detector:Union[OBBCollisionDetector]):
 
-        world_frame =  Box(pos=[0,0,0],length=2,width=2,height=2).wireframe()
+        self.obb_collision_detector = obb_collision_detector
 
-        self.plt = Plotter()
-        self.plt.add(world_frame)
-    def update_obbs_boxes(self,obbs:List[OBB]):
-        for i,obb in enumerate(obbs):
-            self.obbs_boxes[i].update(obb)
+        self.plotter = Plotter()
 
-    def render(self):
-        self.plt.show(*self.obbs_boxes,axes=1,viewup='z',interactive=False)
+        self.world_frame = Box(pos=[0, 0, 0], length=2, width=2, height=2).wireframe()
+        self.plotter.add(self.world_frame)
+
+        self.button = self.plotter.add_button(
+            self._button_func,
+            states=["\u23F5 Play  ", "\u23F8 Pause"],
+            font="Kanopus",
+            size=32,
+        )
+        self.counter = 0
+        self.timer_id =None
+
+        self.plotter.add_callback('timer',self.loop,enable_picking=False)
+        self.plotter.show()
+
+
+    def _button_func(self, obj, ename):
+        if self.timer_id is not None:
+            self.plotter.timer_callback("destroy",self.timer_id)
+        if "Play" in self.button.status():
+            self.timer_id = self.plotter.timer_callback("create",dt=10)
+
+        self.button.switch()
+
+    def loop(self,event):
+        self.counter += 1
+
+        self.obb_collision_detector.update_obbs_transform(
+            global_rotations=torch.tensor([[0,0,0,1],[0,0,0,1]]),
+            global_translations=torch.tensor([[self.counter/1000,0,0],[-self.counter/1000,0,0]]),
+        )
+        self.plotter.clear()
+        obb_boxes = [ObbBox(obb) for obb in self.obb_collision_detector.get_obbs()]
+
+        self.plotter.add(*obb_boxes,self.world_frame)
+        self.plotter.render()
+
 
 
 
@@ -68,16 +121,16 @@ if __name__ == "__main__":
     )
 
     obb_detector = OBBCollisionDetector([obb1,obb2]*20)
-    obbv = ObbVisualizer(obb_detector.get_obbs())
 
+    # obbv = ObbVisualizer(obb_detector.get_obbs())
 
+    # for i in range(1000):
+    #
+    #     obb_detector.update_obbs_transform(
+    #         global_rotations=torch.tensor([[0,0,0,1],[0,0,0,1]]),
+    #         global_translations=torch.tensor([[i/1000,0,0],[-i/1000,0,0]]),
+    #     )
+    #     obbv.update_obbs_boxes(obb_detector.get_obbs())
+    #     obbv.render()
 
-    for i in range(1000):
-
-        obb_detector.update_obbs_transform(
-            global_rotations=torch.tensor([[0,0,0,1],[0,0,0,1]]),
-            global_translations=torch.tensor([[i/1000,0,0],[-i/1000,0,0]]),
-        )
-        obbv.update_obbs_boxes(obb_detector.get_obbs())
-        obbv.render()
-
+    obb_visualizer = ObbVisualizer(obb_detector)
